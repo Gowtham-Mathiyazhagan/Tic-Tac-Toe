c4aa198b90619850697d7ac4d1468da636587664
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2023-12-27 22:05:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+//XO
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+    <div className="title">
+      <div className="title-name">Tic Tac Toe</div> 
+    </div>
+    <div className="players">
+      <div className="player1">Player-1 :</div>
+      <div className="player2">Player-2 :</div>
+    </div>
+    <div className="main-container">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
+    </>
+  );
 }
 
-export default App
+export default App;
